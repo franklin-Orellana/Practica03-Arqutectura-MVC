85a1cb9297239bcd48907d5218a5eeee894ccc2c
Fernanda <Fernanda@HP_Dv6>
2019-04-24 22:56:17 -0500

    Implementación de la interface SortedSet y la clase TreeSet en la clase hija Póker.

--- src/ec/edu/ups/controladores/ControladorPoker.java
+++ src/ec/edu/ups/controladores/ControladorPoker.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ec.edu.ups.controladores;
 
-/**
- *
- * @author Fernanda
- */
+import ec.edu.ups.clases.Poker;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class ControladorPoker {
+
+    private SortedSet<Poker> listaOrdenada;
+    private int codigo;
+
+    public ControladorPoker() {
+    
+        listaOrdenada = new TreeSet<>();
+        codigo = 1;
+    
+    }
+
+    public void create(Poker objeto){
+        codigo ++;
+        objeto.setCodigo(codigo);
+        listaOrdenada.add(objeto);
+    }
+    
+    public Poker read(int codigo){
+        for (Poker poker : listaOrdenada) {
+            if(poker.getCodigo() == codigo){
+                return poker;
+            }
+        }
+        return null;
+    }
+    
+    public void update(Poker objeto){
+        if(listaOrdenada.contains(objeto)){
+            listaOrdenada.remove(objeto);
+            listaOrdenada.add(objeto);
+        }
+    }
+    
+    public void delete(int codigo){
+        for (Poker poker : listaOrdenada) {
+            if(poker.getCodigo() == codigo){
+                listaOrdenada.remove(poker);
+            break;
+            }
+            
+        }
+    }
+
+    public void imprimir(){
+        System.out.println("Lista Ordenada por Nombre");
+        for (Poker poker : listaOrdenada) {
+            System.out.println(poker.getNombre());
+            
+        }
+    }
     
 }
+
+
