ea387aad244d2460760e8a2c4257f9e6870edd4d
Fernanda <Fernanda@HP_Dv6>
2019-04-24 22:51:12 -0500

    implementacion usando la interface Set y la clase HashSet

--- src/ec/edu/ups/controladores/ControladorAventura.java
+++ src/ec/edu/ups/controladores/ControladorAventura.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ec.edu.ups.controladores;
 
-/**
- *
- * @author Fernanda
- */
+import ec.edu.ups.clases.Aventura;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ControladorAventura {
-    
+
+    private List<Aventura> lista;
+    private int contador;
+
+    public ControladorAventura() {
+        lista = new ArrayList<>();
+        contador = 0;
+    }
+
+    public void create(Aventura objeto) {
+        contador++;
+        objeto.setCodigo(contador);
+        lista.add(objeto);
+
+    }
+
+    public Aventura read(int codigo) {
+        for (Aventura aventura : lista) {
+            if (aventura.getCodigo() == codigo) {
+                return aventura;
+            }
+        }
+        return null;
+    }
+
+    public void update(Aventura objeto) {
+        for (int i = 0; i < lista.size(); i++) {
+            Aventura elemento = lista.get(i);
+            if (elemento.getCodigo() == objeto.getCodigo()) {
+                lista.set(i, objeto);
+                break;
+            }
+        }
+    }
+
+    public void delete(Aventura objeto) {
+        for (int i = 0; i < lista.size(); i++) {
+            Aventura elemento = lista.get(i);
+            if (elemento.getCodigo() == objeto.getCodigo()) {
+                lista.remove(i);
+                break;
+            }
+        }
+
+    }
 }
--- src/ec/edu/ups/controladores/ControladorDeporte.java
+++ src/ec/edu/ups/controladores/ControladorDeporte.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ec.edu.ups.controladores;
 
-/**
- *
- * @author Fernanda
- */
+import ec.edu.ups.clases.Deporte;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class ControladorDeporte {
+
+    private Set<Deporte> lista;
+    private int codigo;
     
+    public ControladorDeporte() {
+        lista = new HashSet<>();
+        codigo=0;
+        
+    }
+    
+    public void create(Deporte objeto){
+        codigo++;
+        objeto.setCodigo(codigo);
+        lista.add(objeto);
+                
+    }
+    
+    public Deporte read(int codigo){
+        for (Deporte deporte : lista) {
+            if(deporte.getCodigo() == codigo){
+                return deporte;
+            }
+            
+        }
+        return null;
+    }
+    
+    public void update(Deporte objeto){
+        if(lista.contains(objeto)){
+            lista.remove(objeto);
+            lista.add(objeto);
+        }
+    }
+    
+    public void delete(int codigo){
+        for (Deporte deporte : lista) {
+            if(deporte.getCodigo() == codigo){
+                lista.remove(deporte);
+            }
+            
+        }
+    }
+
 }
