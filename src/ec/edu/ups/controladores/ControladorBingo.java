8b037dbd933170fb5a79780152ddceef9b96bc54
Fernanda <Fernanda@HP_Dv6>
2019-04-24 23:08:44 -0500

    Implementación de la interface Map y la clase HashMap o la interface Map y la clase TreeMap en la clase hija Bingo.

--- src/ec/edu/ups/controladores/ControladorBingo.java
+++ src/ec/edu/ups/controladores/ControladorBingo.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ec.edu.ups.controladores;
 
-/**
- *
- * @author Fernanda
- */
+import ec.edu.ups.clases.Bingo;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class ControladorBingo {
+
+    private Map <Integer, Bingo> lista;
+    private int codigo;
+
+    public ControladorBingo() {
+    
+        lista = new TreeMap<>();
+        codigo = 0;      
+        
+    }
+    
+    public void create(Bingo objeto){
+        codigo++;
+        objeto.setCodigo(codigo);
+        lista.put(codigo, objeto);
+    }
+    
+    public Bingo read(int codigo){
+        if(lista.get(codigo) != null){
+            return lista.get(codigo);
+        }
+        return null;
+    }
+    
+    public void update(Bingo objeto){
+        lista.put(objeto.getCodigo(), objeto);
+        
+    }
+    
+    public void delete(int codigo){
+        lista.remove(codigo);
+    }
+    
+    public void imprimir(){
+        System.out.println(lista.entrySet());
+    }
+    
+    
     
 }
